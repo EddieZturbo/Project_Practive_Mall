95246b8c1de43d01c07cfe2b69402e047e651298
EddieZturbo <zjh20001207@icloud.com>
2022-12-13 17:21:48 +0800

    mall_project_2

---
+++ Mall_Order/src/test/java/com/eddie/mall_order/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eddie.mall_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

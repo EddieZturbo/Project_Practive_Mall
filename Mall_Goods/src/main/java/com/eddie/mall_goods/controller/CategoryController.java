15fb0c769cb18921a40d4df4d703c894a5e7bf11
EddieZturbo <zjh20001207@icloud.com>
2023-01-01 13:00:37 +0800

    截止至:商品服务-api-三级分类-分类完成(p58)

--- Mall_Goods/src/main/java/com/eddie/mall_goods/controller/CategoryController.java
+++ Mall_Goods/src/main/java/com/eddie/mall_goods/controller/CategoryController.java
@@ -60,7 +60,7 @@ public class CategoryController {
     public R info(@PathVariable("catId") Long catId) {
         CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
 
     /**
@@ -70,6 +70,17 @@ public class CategoryController {
     //@RequiresPermissions("mall_goods:category:save")
     public R save(@RequestBody CategoryEntity category) {
         categoryService.save(category);
+
+        return R.ok();
+    }
+
+    /**
+     * 批量修改
+     */
+    @RequestMapping("/update/sort")
+    //@RequiresPermissions("mall_goods:category:update")
+    public R updateSort(@RequestBody CategoryEntity[] category) {
+        categoryService.updateBatchById(Arrays.asList(category));
 
         return R.ok();
     }

1099dafc3c5fd5b839db3085be6bd5b7efd60ff8
EddieZturbo <zjh20001207@icloud.com>
2023-01-10 21:17:11 +0800

    品牌管理 前端数据校验&后端使用JSR303规范(注解方式)进行数据校验(66)

--- Mall_Goods/src/main/java/com/eddie/mall_goods/controller/BrandController.java
+++ Mall_Goods/src/main/java/com/eddie/mall_goods/controller/BrandController.java
@@ -1,9 +1,12 @@
 package com.eddie.mall_goods.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +17,6 @@ import com.eddie.mall_goods.entity.BrandEntity;
 import com.eddie.mall_goods.service.BrandService;
 import com.eddie.common.utils.PageUtils;
 import com.eddie.common.utils.R;
-
 
 
 /**
@@ -35,7 +37,7 @@ public class BrandController {
      */
     @RequestMapping("/list")
     //@RequiresPermissions("mall_goods:brand:list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = brandService.queryPage(params);
 
         return R.ok().put("page", page);
@@ -47,8 +49,8 @@ public class BrandController {
      */
     @RequestMapping("/info/{brandId}")
     //@RequiresPermissions("mall_goods:brand:info")
-    public R info(@PathVariable("brandId") Long brandId){
-		BrandEntity brand = brandService.getById(brandId);
+    public R info(@PathVariable("brandId") Long brandId) {
+        BrandEntity brand = brandService.getById(brandId);
 
         return R.ok().put("brand", brand);
     }
@@ -58,10 +60,20 @@ public class BrandController {
      */
     @RequestMapping("/save")
     //@RequiresPermissions("mall_goods:brand:save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
-
-        return R.ok();
+    //TODO 要使后端的JSR303规范(注解方式)进行数据校验生效 必须在需要校验的参数上加上 @Validated注解 要想获取校验结果 必须紧跟BindingResult result对象
+    public R save(@Validated @RequestBody BrandEntity brand, BindingResult result) {
+        if (result.hasErrors()) {//判断校验是否有错误
+            HashMap<String, String> map = new HashMap<>();
+            result.getFieldErrors().forEach((item) -> {
+                String errorName = item.getField();//错误字段的name
+                String errorMessage = item.getDefaultMessage();//错误的message
+                map.put(errorName,errorMessage);
+            });
+            return R.error(400,"提交的数据不合法").put("data",map);
+        } else {
+            brandService.save(brand);
+            return R.ok();
+        }
     }
 
     /**
@@ -69,10 +81,19 @@ public class BrandController {
      */
     @RequestMapping("/update")
     //@RequiresPermissions("mall_goods:brand:update")
-    public R update(@RequestBody BrandEntity brand){
-		brandService.updateById(brand);
-
-        return R.ok();
+    public R update(@Validated @RequestBody BrandEntity brand, BindingResult result) {
+        if (result.hasErrors()) {//判断校验是否有错误
+            HashMap<String, String> map = new HashMap<>();
+            result.getFieldErrors().forEach((item) -> {
+                String errorName = item.getField();//错误字段的name
+                String errorMessage = item.getDefaultMessage();//错误的message
+                map.put(errorName,errorMessage);
+            });
+            return R.error(400,"提交的数据不合法").put("data",map);
+        } else {
+            brandService.save(brand);
+            return R.ok();
+        }
     }
 
     /**
@@ -80,8 +101,8 @@ public class BrandController {
      */
     @RequestMapping("/delete")
     //@RequiresPermissions("mall_goods:brand:delete")
-    public R delete(@RequestBody Long[] brandIds){
-		brandService.removeByIds(Arrays.asList(brandIds));
+    public R delete(@RequestBody Long[] brandIds) {
+        brandService.removeByIds(Arrays.asList(brandIds));
 
         return R.ok();
     }
--- Mall_Goods/src/main/java/com/eddie/mall_goods/entity/BrandEntity.java
+++ Mall_Goods/src/main/java/com/eddie/mall_goods/entity/BrandEntity.java
@@ -6,10 +6,14 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.*;
 
 /**
  * 品牌
- * 
+ * TODO 后端使用JSR303规范(注解方式)进行数据校验 SpringBoot版本要求不超过2.0
+ * 可以使用javax.validation提供的注解 也可以自定义校验规则RegExp（正则表达式）以及错误提示message
  * @author Eddie
  * @email 20001207@iCloud.com
  * @date 2022-12-15 22:11:48
@@ -27,10 +31,14 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 品牌名
 	 */
+	@NotEmpty
+	@NotBlank(message = "品牌名不能为空")
 	private String name;
 	/**
 	 * 品牌logo地址
 	 */
+	@NotEmpty
+	@URL(message = "品牌LOGO要求必须是一个规范的URL地址")
 	private String logo;
 	/**
 	 * 介绍
@@ -43,10 +51,14 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 检索首字母
 	 */
+	@NotEmpty
+	@Pattern(regexp = "^[a-zA-Z]$",message = "检索首字母必须是单个的字母")
 	private String firstLetter;
 	/**
 	 * 排序
 	 */
+	@NotNull
+	@Min(value = 0,message = "排序必须大于等于0")
 	private Integer sort;
 
 }

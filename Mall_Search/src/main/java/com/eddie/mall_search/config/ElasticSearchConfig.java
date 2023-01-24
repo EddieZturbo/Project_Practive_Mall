be788a8827bf7cd2a0316e5785c210b0f7620c21
EddieZturbo <zjh20001207@icloud.com>
2023-01-25 00:30:31 +0800

    Spring Boot 整合 ElasticSearch （elasticsearch-rest-high-level-client）

---
+++ Mall_Search/src/main/java/com/eddie/mall_search/config/ElasticSearchConfig.java
@@ -0,0 +1,25 @@
+package com.eddie.mall_search.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ @author EddieZhang
+ @create 2023-01-25 12:18 AM
+ */
+@Configuration
+public class ElasticSearchConfig {
+
+    @Bean
+    public RestHighLevelClient restHighLevelClient(){
+        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("192.168.199.180",9200,"http")
+                )
+        );
+        return restHighLevelClient;
+    }
+}

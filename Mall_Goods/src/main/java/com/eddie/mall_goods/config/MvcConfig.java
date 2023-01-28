337fe873f5d94b6c9b0a2ff44b730c09534e49de
EddieZturbo <zjh20001207@icloud.com>
2023-01-28 19:47:54 +0800

    商品业务 首页 整合Thymeleaf渲染首页 完成(136)

---
+++ Mall_Goods/src/main/java/com/eddie/mall_goods/config/MvcConfig.java
@@ -0,0 +1,30 @@
+package com.eddie.mall_goods.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ @author EddieZhang
+ @create 2023-01-28 12:41 PM
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurationSupport {
+    //TODO 静态资源访问配置CLASSPATH_RESOURCE_LOCATIONS
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations(
+                    "classpath:/META-INF/resources/webjars/");
+        }
+        if (!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(
+                    CLASSPATH_RESOURCE_LOCATIONS);
+        }
+    }
+
+}

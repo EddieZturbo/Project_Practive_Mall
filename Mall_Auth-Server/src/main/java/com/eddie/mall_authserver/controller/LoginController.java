9355cac05e197c0ce6ecde9105e4a26852f63ff1
EddieZturbo <zjh20001207@icloud.com>
2023-02-16 14:39:45 +0800

    项目重新打包 暂时跳过了使用SpringSession OAuth2（第三方登录）SSO单点登录 先开展购物车部分的业务

--- Mall_Auth-Server/src/main/java/com/eddie/mall_authserver/controller/LoginController.java
+++ Mall_Auth-Server/src/main/java/com/eddie/mall_authserver/controller/LoginController.java
@@ -11,12 +11,13 @@ import com.eddie.mall_authserver.vo.UserRegisterVo;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
